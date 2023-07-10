f548a0866db2e914cc80632c4ea80d136ac5cd48
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-07-10 22:12:23 +0100

    contact prop type

--- components/contact/index.tsx
+++ components/contact/index.tsx
@@ -1,23 +1,32 @@
-import React from 'react'
+import { useAppSelector } from '@components'
+import { contact } from '@utils/types/customer'
 
-const page = () => {
+const page = (contact: contact) => {
+  const counter = useAppSelector(state => state.counter.value)
   return (
     <div className="contact">
-      <h4>contact</h4>
+      <h4>contact {counter}</h4>
       <div className="item">
         <label htmlFor="name">name</label>
-        <input type="text" />
+        <input type="text" value={contact.name} />
       </div>
       <div className="item">
         <label htmlFor="email">email</label>
-        <input type="text" />
+        <input type="text" value={contact.email} />
       </div>
       <div className="item">
         <label htmlFor="tel">tel</label>
-        <input type="text" />
+        <input type="text" value={contact.tel} />
       </div>
     </div>
   )
 }
 
 export default page
+
+// type contact = {
+//   _id: string
+//   name: string
+//   tel: string
+//   email: string
+// }
