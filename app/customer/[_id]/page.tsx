47783c7d4aea385b56de0d0a4040b4a3392ae844
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-07-11 05:49:32 +0100

    add methodInfo

--- app/customer/[_id]/page.tsx
+++ app/customer/[_id]/page.tsx
@@ -1,26 +1,14 @@
 'use client'
 
-import { Customers as CustType, contact as ContactType } from '@utils/types/customer'
-import {
-  useAppSelector,
-  useEffect,
-  useState,
-  axios,
-  Contact,
-  Show,
-  Hide,
-} from '@components'
+//prettier-ignore
+import {  useAppSelector, useEffect, useState, Contact, Show, Hide, Link, CustType, ContactType, MethodType, Method } from '@components'
 
 const page = ({ params }: { params: CustType }) => {
-  // const customers = useAppSelector(state => state.customer.value)
-  // const customer = customers.filter((cust: CustType) => cust._id === params._id)
-  // const name = customer.map((cust: CustType) => cust.name)
-  // const [customerName, setCustomerName] = useState('')
-
-  const [userPosts, setUserPosts] = useState([])
+  const customers = useAppSelector(state => state.customer.value)
   const [customer, setCustomer] = useState([])
-  const [contact, setContact] = useState<ContactType[]>([])
   const [show, setShow] = useState(false)
+  const [contact, setContact] = useState<ContactType[]>([])
+  const [method, setMethod] = useState<MethodType[]>([])
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -29,6 +17,7 @@ const page = ({ params }: { params: CustType }) => {
       const filteredData = data.filter((c: CustType) => c._id === params._id)[0]
       setCustomer(filteredData)
       setContact(filteredData.contact)
+      setMethod(filteredData.methodInfo)
     }
 
     if (params?._id) fetchPosts()
@@ -36,7 +25,13 @@ const page = ({ params }: { params: CustType }) => {
 
   return (
     <>
-      <div>page</div>
+      <div className="flex gap-1 flex-wrap">
+        {customers.map((cust: CustType) => (
+          <Link key={cust._id} href={`/customer/${cust._id}`}>
+            {cust.name}
+          </Link>
+        ))}
+      </div>
       {params._id} - {customer.length}
       <hr />
       {show ? (
@@ -46,9 +41,19 @@ const page = ({ params }: { params: CustType }) => {
       )}
       {show && <pre>{JSON.stringify(customer, null, 2)}</pre>}
       <hr />
+      {method.map(meth => (
+        <Method
+          key={meth._id}
+          methodName={meth.methodName}
+          url={meth.url}
+          username={meth.username}
+          password={meth.password}
+          notes={meth.password}
+        />
+      ))}
       <hr />
       {contact.map(cont => (
-        <Contact name={cont.name} email={cont.email} tel={cont.tel} _id={cont._id} />
+        <Contact key={cont._id} name={cont.name} email={cont.email} tel={cont.tel} />
       ))}
     </>
   )
