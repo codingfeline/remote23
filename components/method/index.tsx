1eccd461f7a91b786422b4a7006b7bfc84d3fa5c
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-07-11 05:49:09 +0100

    prop method: MethodTy[e

--- components/method/index.tsx
+++ components/method/index.tsx
@@ -1,17 +1,28 @@
-import { UserPass } from '@components'
-import React from 'react'
+// prettier-ignore
+import { Copy, Edit, Plus, UserPass, useAppDispatch, increment, MethodType } from '@components'
 
-const page = () => {
+const page = (method: MethodType) => {
+  const dispatch = useAppDispatch()
   return (
     <div className="method">
-      <h4>method</h4>
+      <div className="flex justify-between">
+        <Edit />
+        <Plus onClick={() => dispatch(increment())} />
+      </div>
       <div className="item">
         <label htmlFor="name">name</label>
-        <input type="text" />
+        <input type="text" id="name" value={method.methodName} />
+        <Copy />
       </div>
       <div className="item">
         <label htmlFor="url">URL</label>
-        <input type="text" />
+        <input type="text" value={method.url} />
+      </div>
+      <div className="item">
+        <label htmlFor="notes"></label>
+        <textarea name="" id="notes" cols={30} rows={3}>
+          {method.notes}
+        </textarea>
       </div>
       <UserPass />
     </div>
