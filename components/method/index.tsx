4a7966a0e329e0381495a17c865ae797cbf6e236
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-07-11 22:00:44 +0100

    reuse UserPass

--- components/method/index.tsx
+++ components/method/index.tsx
@@ -1,7 +1,7 @@
 // prettier-ignore
-import { Copy, Edit, Plus, UserPass, useAppDispatch, increment, MethodType } from '@components'
+import { Copy, Edit, Plus, UserPass, useAppDispatch, increment, MethodInfoType } from '@components'
 
-const page = (method: MethodType) => {
+const page = (method: MethodInfoType) => {
   const dispatch = useAppDispatch()
   return (
     <div className="method">
@@ -12,19 +12,19 @@ const page = (method: MethodType) => {
       <div className="item">
         <label htmlFor="name">name</label>
         <input type="text" id="name" value={method.methodName} />
-        <Copy />
+        <Copy onClick={() => alert([method._id, method.methodName])} />
       </div>
       <div className="item">
         <label htmlFor="url">URL</label>
         <input type="text" value={method.url} />
       </div>
+      <UserPass username={method.username} password={method.password} />
       <div className="item">
         <label htmlFor="notes"></label>
         <textarea name="" id="notes" cols={30} rows={3}>
           {method.notes}
         </textarea>
       </div>
-      <UserPass />
     </div>
   )
 }
