4678c655f97fcf794a26bbbb6a6130968a370942
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-07-16 20:14:37 +0100

    add classname

--- components/server/index.tsx
+++ components/server/index.tsx
@@ -4,14 +4,14 @@ const page = ({ server }: { server: ServerType[] }) => {
   const [showServer, setshowServer] = useState(true)
 
   return (
-    <>
+    <div className="server">
       <div className="groupMaster group" onClick={() => setshowServer(prev => !prev)}>
         {showServer ? <Show className="groupSub" /> : <Hide className="groupSub" />}
         <span className="groupSub">Server</span>
       </div>
       <div className={`transIn ${!showServer && 'transOut'}`}>
         {server.map(serv => (
-          <div className="server bg-lime-100">
+          <div className=" bg-lime-100 sub">
             <div className="item">
               <label htmlFor="name">name</label>
               <input type="text" value={serv.name} readOnly />
@@ -24,7 +24,7 @@ const page = ({ server }: { server: ServerType[] }) => {
           </div>
         ))}
       </div>
-    </>
+    </div>
   )
 }
 
