3bbd8476154509ef5d973f6cb2ddda1876147be0
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-07-16 12:47:52 +0100

    refactored server

--- components/server/index.tsx
+++ components/server/index.tsx
@@ -1,18 +1,30 @@
-import { UserPass, ServerType } from '@components'
+import { UserPass, ServerType, useState, Show, Hide } from '@components'
 
-const page = (server: ServerType) => {
+const page = ({ server }: { server: ServerType[] }) => {
+  const [showServer, setshowServer] = useState(true)
+
   return (
-    <div className="server bg-lime-100">
-      <div className="item">
-        <label htmlFor="name">name</label>
-        <input type="text" value={server.name} />
+    <>
+      <div className="groupMaster group" onClick={() => setshowServer(prev => !prev)}>
+        {showServer ? <Show className="groupSub" /> : <Hide className="groupSub" />}
+        <span className="groupSub">Server</span>
       </div>
-      <div className="item">
-        <label htmlFor="ip">IP</label>
-        <input type="text" value={server.ip} />
+      <div className={`transIn ${!showServer && 'transOut'}`}>
+        {server.map(serv => (
+          <div className="server bg-lime-100">
+            <div className="item">
+              <label htmlFor="name">name</label>
+              <input type="text" value={serv.name} readOnly />
+            </div>
+            <div className="item">
+              <label htmlFor="ip">IP</label>
+              <input type="text" value={serv.ip} />
+            </div>
+            <UserPass username={serv.username} password={serv.password} />
+          </div>
+        ))}
       </div>
-      <UserPass username={server.username} password={server.password} />
-    </div>
+    </>
   )
 }
 
