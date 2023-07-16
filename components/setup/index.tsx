f14bf6b7bc576fb3fecb31b7f79be2612e087e05
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-07-16 20:14:12 +0100

    refactored setup

--- components/setup/index.tsx
+++ components/setup/index.tsx
@@ -1,22 +1,33 @@
-import { ServerSetupType } from '@utils/types/customer'
+import { ServerSetupType, useState, Show, Hide } from '@components'
 
-const index = (setup: ServerSetupType) => {
+const index = ({ setup }: { setup: ServerSetupType[] }) => {
+  const [showSetup, setshowSetup] = useState(true)
+
   return (
-    <>
-      <div className="item">
-        <label htmlFor="comment">comment</label>
-        <input type="text" value={setup.comment} />
+    <div className="setup">
+      <div className="groupMaster group" onClick={() => setshowSetup(prev => !prev)}>
+        {showSetup ? <Show className="groupSub" /> : <Hide className="groupSub" />}
+        <span className="groupSub">Screenshots</span>
       </div>
-      <div className="item">
-        <label htmlFor="screenshot">screenshot</label>
-        <img
-          src={`https://remoteapi.nazs.net/screenshots/${setup.screenshot}`}
-          alt="screenshot"
-          width={200}
-          height="auto"
-        />
+      <div className={`transIn ${!showSetup && 'transOut'}`}>
+        {setup.map(set => (
+          <div className="bg-lime-100 sub">
+            <div className="item">
+              <label htmlFor="comment">comment</label>
+              <input type="text" value={set.comment} />
+            </div>
+            <div className="item"></div>
+            <label htmlFor="screenshot">screenshot</label>
+            <img
+              src={`https://remoteapi.nazs.net/screenshots/${set.screenshot}`}
+              alt="screenshot"
+              width={200}
+              height="auto"
+            />
+          </div>
+        ))}
       </div>
-    </>
+    </div>
   )
 }
 
