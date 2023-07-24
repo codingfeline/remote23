17befabed697904d9416e46f1540d3a293876ef6
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-07-24 22:45:32 +0100

    refactor

--- app/cust/[_id]/page.tsx
+++ app/cust/[_id]/page.tsx
@@ -5,7 +5,7 @@ import {  useAppSelector, useAppDispatch, useEffect, useState, Contact, Show, Hi
 const cust = ({ params }: { params: CustomerType }) => {
   const dispatch = useAppDispatch()
   const customers = useAppSelector(state => state.customer.customers)
-  const customer = customers.filter((cust: CustomerType) => cust._id == params._id)
+  const cust = customers.filter((cust: CustomerType) => cust._id == params._id)[0]
   const copy = useAppSelector(state => state.copy.value)
   const [show, setShow] = useState(false)
 
@@ -34,22 +34,20 @@ const cust = ({ params }: { params: CustomerType }) => {
         ))}
         <Link href="/">Home</Link>
       </div>
-      {JSON.stringify(customer[0].name)} --name-
-      {JSON.stringify(customer[0].contact)} --contact-
-      {params._id} - {customer[0].name}
+      {cust._id}, {cust.name}
       <hr />
       {show ? (
         <Hide onClick={() => setShow(prev => !prev)} />
       ) : (
         <Show onClick={() => setShow(prev => !prev)} />
       )}
-      {show && <pre>{JSON.stringify(customer, null, 2)}</pre>}
+      {show && <pre>{JSON.stringify(cust, null, 2)}</pre>}
       <hr />
-      <Method method={customer[0].methodInfo} />
-      <Contact contact={customer[0].contact} />
-      <Server server={customer[0].server} />
-      <Device device={customer[0].devicePassword} />
-      <Setup setup={customer[0].serverSetup} />
+      <Method method={cust.methodInfo} />
+      <Contact contact={cust.contact} />
+      <Server server={cust.server} />
+      <Device device={cust.devicePassword} />
+      <Setup setup={cust.serverSetup} />
     </>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,41 +1,35 @@
 'use client'
 //prettier-ignore
-import { Contact, Device, Link, Method, Server, useAppDispatch, useAppSelector, useEffect, axios } from '@components'
-import { Customers as CustType } from '@utils/types/customer'
-import { setCustomers } from '@redux/features/customers/customerSlice'
+import { Link, useAppDispatch, useAppSelector, useEffect, fetchCustomers } from '@components'
 
 export default function Home() {
-  const counter = useAppSelector(state => state.counter.value)
-  const customers = useAppSelector(state => state.customer.value)
   const dispatch = useAppDispatch()
+  const counter = useAppSelector(state => state.counter.value)
+  const customers = useAppSelector(state => state.customer.customers)
+  const customer = customers[0]
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get('https://remoteapi.nazs.net/api/customers')
-        const customers = response.data
-        dispatch(setCustomers(customers))
-      } catch (error) {
-        console.error('Error fetching customers:', error)
-      }
-    }
-
-    fetchData()
-  }, [dispatch])
+    dispatch(fetchCustomers())
+  }, [])
 
   return (
     <main className="flex flex-col">
       home
+      {/* {JSON.stringify(customer)} -- filtered */}
       {counter}
-      <div className="flex gap-1 flex-wrap">
+      {/* <div className="flex gap-1 flex-wrap">
         {customers.map((cust: CustType) => (
           <Link href={`/customer/${cust._id}`}>{cust.name}</Link>
         ))}
+      </div> */}
+      <div className="flex gap-1 flex-wrap">
+        {customers.map(cust => (
+          <Link key={cust._id} href={`/cust/${cust._id}`}>
+            {cust.name}
+          </Link>
+        ))}
       </div>
-      <div className="flex flex-col">
-        <h4>Method</h4>
-        <div className="flex "></div>
-      </div>
+      <hr />
     </main>
   )
 }
