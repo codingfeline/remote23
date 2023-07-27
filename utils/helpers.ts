2bb4f12c190bbffcd3123de48cff38ae26cd9ff9
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-07-27 07:16:43 +0100

    helper arrays

---
+++ utils/helpers.ts
@@ -0,0 +1,7 @@
+export const solutions: string[] = [
+  'Equitrac', 'PaperCut', 'Safecom', 'EOP', 'Cirrato', 'Other', 'None'
+]
+
+export const vpns: string[] = [
+  'Cisco AnyConnect', 'VMWare', 'FortiClient', 'Other', 'GlobalProtect', 'TeamViewer'
+]
