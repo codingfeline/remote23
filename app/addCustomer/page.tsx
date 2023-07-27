035556c300b8f2d72277702790a954fd3d54b9ce
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-07-27 20:58:25 +0100

    with reusable Input

--- app/addCustomer/page.tsx
+++ app/addCustomer/page.tsx
@@ -8,6 +8,15 @@ const AddCustomer = () => {
   const [name, setName] = useState<string>('')
   const [solution, setSolution] = useState<string>('')
   const [method, setMethod] = useState<string>('')
+  const [itName, setItName] = useState<string>('')
+  const [email, setEmail] = useState<string>('')
+  const [tel, setTel] = useState<string>('')
+  const [data, setData] = useState({
+    name1: '',
+    solution1: '',
+    method1: '',
+    itName1: '',
+  })
   vpns.sort()
 
   const radioChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -17,38 +26,57 @@ const AddCustomer = () => {
   return (
     <>
       <h3>AddCustomer</h3>
-      <fieldset>
-        <legend>Customer</legend>
-        <div className="item">
-          <label htmlFor="name">name</label>
-          <Input value={name} handleChange={e => setName(e.target.value)} />
-        </div>
-        <div className="item">
-          <label htmlFor="solution">solution</label>
-          <select
-            name="solution"
-            id="solution"
-            onChange={e => setSolution(e.target.value)}
-          >
-            {solutions.map(solution => (
-              <option key={solution} value={solution}>
-                {solution}
-              </option>
+      <form action="">
+        <fieldset>
+          <legend>Customer</legend>
+          <Input name="name" value={name} handleChange={e => setName(e.target.value)} />
+          <div className="item">
+            <label htmlFor="solution">solution</label>
+            <select
+              name="solution"
+              id="solution"
+              onChange={e => setSolution(e.target.value)}
+            >
+              <option value="">Choose one</option>
+              {solutions.map(solution => (
+                <option key={solution} value={solution}>
+                  {solution}
+                </option>
+              ))}
+            </select>
+          </div>
+        </fieldset>
+        <fieldset>
+          <legend>Remote Method</legend>
+          <div className="item flex flex-wrap" onChange={radioChange}>
+            {vpns.map(vpn => (
+              <label htmlFor={vpn} className="radioLabel">
+                <input type="radio" id={vpn} name="method" value={vpn} />
+                {vpn}
+              </label>
             ))}
-          </select>
+          </div>
+        </fieldset>
+        {method === 'TeamViewer' && (
+          <fieldset>
+            <legend>IT Contact</legend>
+            <Input
+              name="IT Name"
+              value={itName}
+              handleChange={e => setItName(e.target.value)}
+            />
+            <Input name="tel" value={tel} handleChange={e => setTel(e.target.value)} />
+            <Input
+              name="email"
+              value={email}
+              handleChange={e => setEmail(e.target.value)}
+            />
+          </fieldset>
+        )}
+        <div className="flex justify-around">
+          <button>Submit</button>
         </div>
-      </fieldset>
-      <fieldset>
-        <legend>Remote Method</legend>
-        <div className="item flex flex-wrap" onChange={radioChange}>
-          {vpns.map(vpn => (
-            <label htmlFor={vpn} className="radioLabel">
-              <input type="radio" id={vpn} name="method" value={vpn} />
-              {vpn}
-            </label>
-          ))}
-        </div>
-      </fieldset>
+      </form>
       <div>
         {name}, {solution}, {method}
       </div>
