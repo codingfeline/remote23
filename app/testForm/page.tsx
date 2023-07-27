1e5571cdbaa99f5f14a9cac0a3b9e58ca4f37f66
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-07-27 20:56:31 +0100

    parent of reusable form

---
+++ app/testForm/page.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import React from 'react'
+import ReusableForm from '@components/ReusableForm'
+
+interface FormValues {
+  name: string
+  email: string
+  message: string
+}
+
+const MyFormComponent: React.FC = () => {
+  const handleSubmitForm = (data: FormValues) => {
+    // Do something with the form data, such as sending it to a server
+    console.log(data)
+  }
+
+  return (
+    <div>
+      <h2>My Form</h2>
+      <ReusableForm onSubmit={handleSubmitForm} />
+    </div>
+  )
+}
+
+export default MyFormComponent
