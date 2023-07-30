2a54f42960a387e8abc09c82e9e618152bd5eab6
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-07-30 18:56:38 +0100

    complete layout with UseForm

--- app/addDevice/[_id]/page.tsx
+++ app/addDevice/[_id]/page.tsx
@@ -1,13 +1,44 @@
-import { CustomerType } from '@components'
-import UseBack from '@components/useBack'
+'use client'
+
+import { CustomerType, UseBack, TextInput, UseForm } from '@components'
 
 const AddContact = ({ params }: { params: CustomerType }) => {
+  const { formData, handleInputChange, handleSubmit } = UseForm({
+    name: '',
+    username: '',
+    password: '',
+  })
+
   return (
     <>
       <h5>Add Device</h5>
-      <form action="">{/* <TextInput  /> */}</form>
-      {params._id}
+      <form onSubmit={handleSubmit}>
+        <fieldset>
+          <legend>IT Contact</legend>
+          <TextInput
+            name="name"
+            value={formData.name}
+            required
+            onChange={handleInputChange}
+          />
+          <TextInput
+            name="username"
+            value={formData.username}
+            required
+            onChange={handleInputChange}
+          />
+          <TextInput
+            type="password"
+            name="password"
+            value={formData.password}
+            required
+            onChange={handleInputChange}
+          />
+        </fieldset>
+        <button type="submit">Submit</button>
+      </form>
       <UseBack />
+      {JSON.stringify(formData)}
     </>
   )
 }
