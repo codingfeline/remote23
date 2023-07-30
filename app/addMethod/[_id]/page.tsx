91fe6006b8a9fba4931ca7222fb080a5c26ad30c
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-07-30 18:57:56 +0100

    form with Hooks

--- app/addMethod/[_id]/page.tsx
+++ app/addMethod/[_id]/page.tsx
@@ -1,16 +1,59 @@
-import { CustomerType } from '@components'
-import UseBack from '@components/useBack'
+'use client'
 
-type one = {
-  _id: string
-}
+import { CustomerType, TextArea, TextInput, UseBack, UseForm } from '@components'
 
-const AddContact = ({ params }: { params: one }) => {
+const AddContact = ({ params }: { params: CustomerType }) => {
+  const { formData, handleInputChange, handleSubmit } = UseForm({
+    name: '',
+    url: '',
+    username: '',
+    password: '',
+    note: '',
+  })
+
   return (
     <>
       <h5>Add Method</h5>
-      <form action="">{/* <TextInput  /> */}</form>
-      {params._id}
+      <form onSubmit={handleSubmit}>
+        <fieldset>
+          <legend>Remote Method</legend>
+          <TextInput
+            name="name"
+            value={formData.name}
+            onChange={handleInputChange}
+            required
+          />
+          <TextInput
+            name="url"
+            value={formData.url}
+            onChange={handleInputChange}
+            required
+          />
+          <TextInput
+            name="username"
+            value={formData.username}
+            onChange={handleInputChange}
+            required
+          />
+          <TextInput
+            name="password"
+            value={formData.password}
+            onChange={handleInputChange}
+            required
+          />
+          <TextArea
+            name="note"
+            value={formData.name}
+            required
+            onChange={handleInputChange}
+          />
+        </fieldset>
+        <div>
+          <button type="submit" className="link">
+            Submit
+          </button>
+        </div>
+      </form>
       <UseBack />
     </>
   )
