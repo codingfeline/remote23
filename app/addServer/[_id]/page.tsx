45874475a3d00bee4a328bd4677d1486f8150617
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-07-30 18:57:34 +0100

    form with all Hooks

--- app/addServer/[_id]/page.tsx
+++ app/addServer/[_id]/page.tsx
@@ -1,12 +1,52 @@
-import { CustomerType } from '@components'
-import UseBack from '@components/useBack'
+'use client'
+
+import { CustomerType, TextInput, UseBack, UseForm } from '@components'
 
 const AddContact = ({ params }: { params: CustomerType }) => {
+  const { formData, handleInputChange, handleSubmit } = UseForm({
+    name: '',
+    ip: '',
+    username: '',
+    password: '',
+  })
+
   return (
     <>
       <h5>Add Server</h5>
-      <form action="">{/* <TextInput  /> */}</form>
-      {params._id}
+      <form onSubmit={handleSubmit}>
+        <fieldset>
+          <legend>Remote Method</legend>
+          <TextInput
+            name="name"
+            value={formData.name}
+            onChange={handleInputChange}
+            required
+          />
+          <TextInput
+            name="ip"
+            value={formData.ip}
+            onChange={handleInputChange}
+            required
+          />
+          <TextInput
+            name="username"
+            value={formData.username}
+            onChange={handleInputChange}
+            required
+          />
+          <TextInput
+            name="password"
+            value={formData.password}
+            onChange={handleInputChange}
+            required
+          />
+        </fieldset>
+        <div>
+          <button type="submit" className="link">
+            Submit
+          </button>
+        </div>
+      </form>
       <UseBack />
     </>
   )
