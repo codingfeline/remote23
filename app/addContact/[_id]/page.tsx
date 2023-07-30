85d8e03287a2afd7ca0c478053e3b76b6f041548
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-07-30 18:58:36 +0100

    form with Hooks

--- app/addContact/[_id]/page.tsx
+++ app/addContact/[_id]/page.tsx
@@ -1,13 +1,44 @@
-import { CustomerType } from '@components'
-import UseBack from '@components/useBack'
+'use client'
+
+import { CustomerType, UseForm, UseBack, TextInput } from '@components'
 
 const AddContact = ({ params }: { params: CustomerType }) => {
+  const { formData, handleInputChange, handleSubmit } = UseForm({
+    name: '',
+    tel: '',
+    email: '',
+  })
+
   return (
     <>
       <h5>Add Contact</h5>
-      <form action="">{/* <TextInput  /> */}</form>
-      {params._id}
+      <form onSubmit={handleSubmit}>
+        <fieldset>
+          <legend>IT Contact</legend>
+          <TextInput
+            name="name"
+            value={formData.name}
+            required
+            onChange={handleInputChange}
+          />
+          <TextInput
+            name="tel"
+            value={formData.tel}
+            required
+            onChange={handleInputChange}
+          />
+          <TextInput
+            type="email"
+            name="email"
+            value={formData.email}
+            required
+            onChange={handleInputChange}
+          />
+        </fieldset>
+        <button type="submit">Submit</button>
+      </form>
       <UseBack />
+      {JSON.stringify(formData)}
     </>
   )
 }
