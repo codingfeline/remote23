ddcfe649889827d1daff23aa5063bde73c3092fc
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-08-07 21:35:28 +0100

    reusable with any endpoint

--- components/HandleForm.tsx
+++ components/HandleForm.tsx
@@ -22,8 +22,9 @@ const UseForm = (initialFormData: FormData) => {
 
   const handleSubmit = (event: FormEvent) => {
     event.preventDefault()
+    console.log(formData)
     const _id = JSON.parse(localStorage.getItem('_id') || '')
-    const url = `http://localhost:3121/api/customers/${_id}/insertOne${formData.api}`
+    const url = `http://localhost:3121/api/customers/${_id}/${formData.api}`
     axios
       .put(url, formData)
       .then(res => {
