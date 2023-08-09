a9a29923d44488a22e85c3468818490cc2638049
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-08-09 21:59:56 +0100

    customer name, _id compo

---
+++ components/CustomerName.tsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react'
+import { CustomerType } from '@utils/types/customer'
+
+const CustomerName = ({ id }: { id: string }) => {
+  const [data, setData] = useState<CustomerType>({} as CustomerType)
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(`http://localhost:3121/api/customers/nameId`)
+      if (response.ok) {
+        const data = await response.json()
+        setData(data.filter((c: CustomerType) => c._id === id)[0])
+      } else {
+        console.error('Failed to fetch data from API')
+      }
+    } catch (error) {
+      console.error('Error fetching data from API:', error)
+    }
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  return (
+    <>
+      <div>Editing Method for {data.name}</div>
+    </>
+  )
+}
+
+export default CustomerName
