27aa5dd2797a800f60d46e0111a032cc538f65de
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-08-12 16:25:03 +0100

    new prop - cid for ToggleHook

--- components/method/index.tsx
+++ components/method/index.tsx
@@ -1,8 +1,11 @@
 // prettier-ignore
-import { Edit, UserPass, MethodInfoType, useState, ToggleHook, useCopyHook, Plus } from '@components'
+import { Edit, UserPass, MethodInfoType, useState, ToggleHook, useEffect } from '@components'
+import URL from '@components/Url'
+import { useRouter } from 'next/navigation'
 
-const method = ({ method }: { method: MethodInfoType[] }) => {
-  const [showMethod, setshowMethod] = useState<boolean>(method.length ? true : false)
+const method = ({ method, cid }: { method: MethodInfoType[]; cid: string }) => {
+  const router = useRouter()
+  const [showMethod, setshowMethod] = useState(true)
 
   const handleShowChange = (show: boolean) => {
     setshowMethod(show)
@@ -15,29 +18,23 @@ const method = ({ method }: { method: MethodInfoType[] }) => {
         name="Method"
         length={method.length}
         compo="addMethod"
+        cid={cid}
       />
       <div className={`transIn ${!showMethod && 'transOut'}`}>
         {method.map(meth => {
-          const Url = useCopyHook(meth.url)
+          const id = meth._id || ''
           const space = (
-            <Plus className="text-yellow-100 hover:text-yellow-100 disabled" />
+            <Edit className="text-yellow-100 hover:text-yellow-100 disabled" />
           )
 
           return (
             <div key={meth._id} className="sub">
               <div className=" bg-yellow-100">
                 <div className="flex justify-between">
-                  <Edit />
+                  <Edit onClick={() => router.push(`/editMethod/${cid}/${id}`)} />
+                  <span className="tracking-[4px] bold">{meth.methodName}</span>
                 </div>
-                <div className="item">
-                  <label htmlFor="name">name</label>
-                  <input type="text" id="name" value={meth.methodName} readOnly />
-                  {space}
-                </div>
-                <div className="item">
-                  <label htmlFor="url">URL</label>
-                  {Url}
-                </div>
+                <URL url={meth.url} />
                 <UserPass username={meth.username} password={meth.password} />
                 <div className="item">
                   <label htmlFor="notes">note</label>
