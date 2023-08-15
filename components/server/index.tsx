8d4faaf5210541641b814b6363b5e80885c2ab4b
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-08-15 23:19:06 +0100

    add edit link

--- components/server/index.tsx
+++ components/server/index.tsx
@@ -1,7 +1,9 @@
-import { UserPass, ServerType, useState, ToggleHook, Plus } from '@components'
+import { UserPass, ServerType, useState, ToggleHook, Plus, Edit } from '@components'
+import { useRouter } from 'next/navigation'
 
-const page = ({ server }: { server: ServerType[] }) => {
-  const [showServer, setshowServer] = useState(server.length ? true : false)
+const page = ({ server, cid }: { server: ServerType[]; cid: string }) => {
+  const router = useRouter()
+  const [showServer, setshowServer] = useState(true)
 
   const handleShowChange = (show: boolean) => {
     setshowServer(show)
@@ -14,17 +16,22 @@ const page = ({ server }: { server: ServerType[] }) => {
         name="Server"
         length={server.length}
         compo="addServer"
+        cid={cid}
       />
       <div className={`transIn ${!showServer && 'transOut'}`}>
         {server.map(serv => {
           const space = <Plus className="text-lime-100 hover:text-lime-100 disabled" />
-
+          const id = serv._id
           return (
             <div key={serv._id} className="sub">
               <div className=" bg-lime-100">
+                <div className="flex justify-between">
+                  <Edit onClick={() => router.push(`/editServer/${cid}/${id}`)} />
+                </div>
                 <div className="item">
                   <label htmlFor="name">name</label>
                   <input type="text" value={serv.name} readOnly />
+
                   {space}
                 </div>
                 <div className="item">
