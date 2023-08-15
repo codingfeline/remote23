c371d8da0ac1583ce54edf57a53d0bccb598fd3a
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-08-15 23:10:23 +0100

    select customers from a sorted list

--- app/cust/[_id]/page.tsx
+++ app/cust/[_id]/page.tsx
@@ -2,48 +2,74 @@
 //prettier-ignore
 import {  useAppSelector, useAppDispatch, useEffect, useState, Contact, Show, Hide, Link, CustomerType, Method, Server, Device, Setup, fetchCustomers } from '@components'
 import CustomerName from '@components/CustomerName'
+import NetworkConfig from '@components/network'
+import { useRouter } from 'next/navigation'
+import Select, { OptionTypeBase } from 'react-select'
 
 const cust = ({ params }: { params: CustomerType }) => {
   const [show, setShow] = useState(false)
   const dispatch = useAppDispatch()
   const customers = useAppSelector(state => state.customer.customers)
   const cust = customers.filter((cust: CustomerType) => cust._id === params._id)[0]
-  const copy = useAppSelector(state => state.copy.value)
-  // localStorage.setItem('_id', JSON.stringify(params._id))
   const cid = params._id || ''
+  const router = useRouter()
+  const [selectedOption, setSelectedOption] = useState<Option | null>(null)
+  const options: Option[] = customers
+    .map(c => {
+      return {
+        label: c.name + ' (' + c.solution + ')',
+        value: c._id,
+      }
+    })
+    .sort((a, b) => {
+      let fa = a.label.toLowerCase(),
+        fb = b.label.toLowerCase()
+
+      if (fa < fb) {
+        return -1
+      }
+      if (fa > fb) {
+        return 1
+      }
+      return 0
+    })
 
   useEffect(() => {
     dispatch(fetchCustomers())
-    // localStorage.setItem('_id', JSON.stringify(params._id))
   }, [])
 
   if (!customers.length) {
     return 'Loading...'
   }
 
+  interface Option {
+    value: string
+    label: string
+  }
+
+  const handleChange = (selected: OptionTypeBase<Option> | null) => {
+    setSelectedOption(selected)
+    if (selected) {
+      router.push(`./${selected.value}`)
+    }
+  }
+
   return (
     <>
-      {' '}
-      <span className={`transition-opacity ${copy ? 'opacity-1' : ' opacity-0'}`}>
-        {copy && (
-          <>
-            Copied <i>{copy}</i>
-          </>
-        )}
-      </span>
-      <div className="links">
-        {customers.map(cust => (
-          <Link key={cust._id} href={`./${cust._id}`}>
-            {cust.name}
-          </Link>
-        ))}
+      <Select<Option>
+        className="w-1/2 m-auto my-1"
+        value={selectedOption}
+        options={options}
+        onChange={handleChange}
+      />
+      <div className="flex justify-center">
+        <span className="bg-blue-200 p-3 rounded-l-md border border-blue-300 border-r-0">
+          <CustomerName id={params._id || ''} />
+        </span>
+        <span className="bg-blue-100 p-3 rounded-r-md border border-blue-300 border-l-0">
+          {cust.solution}
+        </span>
       </div>
-      {/* {cust._id}, {cust.name} */}
-      Customer:
-      <span className="ml-2">
-        <CustomerName id={params._id || ''} />
-      </span>
-      <hr />
       {show ? (
         <Hide onClick={() => setShow(prev => !prev)} />
       ) : (
@@ -51,6 +77,7 @@ const cust = ({ params }: { params: CustomerType }) => {
       )}
       {show && <pre>{JSON.stringify(cust, null, 2)}</pre>}
       <hr />
+      <NetworkConfig />
       <Method method={cust.methodInfo} cid={cid} />
       <Contact contact={cust.contact} cid={cid} />
       <Server server={cust.server} cid={cid} />
