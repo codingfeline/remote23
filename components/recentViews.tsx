eca532b7dd0236922f7e53568fc7307e51e1cbfb
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-08-16 18:46:54 +0100

    put item from recent to top when viewed again

--- components/recentViews.tsx
+++ components/recentViews.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import Link from 'next/link'
 import { MdClose } from 'react-icons/md'
+import { useRouter } from 'next/navigation'
 
 interface Option {
   value: string
@@ -8,6 +9,7 @@ interface Option {
 }
 
 const RecentViews = () => {
+  const router = useRouter()
   const [recent, setRecent] = useState<Option[]>([])
 
   useEffect(() => {
@@ -25,16 +27,25 @@ const RecentViews = () => {
     localStorage.setItem('recent', JSON.stringify(newItems))
   }
 
+  const goRecent = (r_item: Option) => {
+    const store = localStorage.getItem('recent')
+    const curr = JSON.parse(store)
+    //remove the item from current list
+    const newArr = curr.filter((item: Option) => item.value !== r_item.value)
+    // add the item to the top of the list
+    newArr.unshift(r_item)
+    localStorage.setItem('recent', JSON.stringify(newArr))
+    router.push(`/cust/${r_item.value}`)
+  }
+
   return (
-    <div className="flex flex-col w-[220px] bg-lime-50">
+    <div className="flex flex-col w-[220px] bg-lime-50 rounded-md p-3 ">
       <h5>Recent Views</h5>
       <div className="links  items-start">
         {recent.map(c => {
           return (
             <div className="flex flex-row w-full justify-between items-center">
-              <Link className="w-full" href={`/cust/${c.value}`}>
-                {c.label}
-              </Link>
+              <button onClick={() => goRecent(c)}>{c.label}</button>
               <MdClose onClick={() => removeRecent(c.value)} />
             </div>
           )
