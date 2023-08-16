12b914b184c7c04aba1fd2a7d453e0b4c9257301
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-08-16 08:11:46 +0100

    using reusable select

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
 'use client'
 //prettier-ignore
 import { Link, useAppDispatch, useAppSelector, useEffect, fetchCustomers } from '@components'
+import SelectCustomer from '@components/selectCustomer'
 
 export default function Home() {
   const dispatch = useAppDispatch()
-  const counter = useAppSelector(state => state.counter.value)
-  const customers = useAppSelector(state => state.customer.customers)
-  const customer = customers[0]
 
   useEffect(() => {
     dispatch(fetchCustomers())
@@ -14,21 +12,7 @@ export default function Home() {
 
   return (
     <main className="flex flex-col">
-      home
-      {/* {JSON.stringify(customer)} -- filtered */}
-      {counter}
-      {/* <div className="flex gap-1 flex-wrap">
-        {customers.map((cust: CustType) => (
-          <Link href={`/customer/${cust._id}`}>{cust.name}</Link>
-        ))}
-      </div> */}
-      <div className="flex gap-1 flex-wrap">
-        {customers.map(cust => (
-          <Link key={cust._id} href={`/cust/${cust._id}`}>
-            {cust.name}
-          </Link>
-        ))}
-      </div>
+      <SelectCustomer />
       <hr />
     </main>
   )
