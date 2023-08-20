ad07ce166e321350afc58e70997ab8215d390b37
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-08-20 14:43:51 +0100

    baseUrl axios

--- components/CustomerName.tsx
+++ components/CustomerName.tsx
@@ -1,21 +1,18 @@
 import React, { useEffect, useState } from 'react'
 import { CustomerType } from '@utils/types/customer'
+import axios from '@components/axios'
 
 const CustomerName = ({ id }: { id: string }) => {
   const [data, setData] = useState<CustomerType>({} as CustomerType)
 
   const fetchData = async () => {
-    try {
-      const response = await fetch(`http://localhost:3121/api/customers/nameId`)
-      if (response.ok) {
-        const data = await response.json()
-        setData(data.filter((c: CustomerType) => c._id === id)[0])
-      } else {
-        console.error('Failed to fetch data from API')
-      }
-    } catch (error) {
-      console.error('Error fetching data from API:', error)
-    }
+    await axios
+      .get('customers/nameId')
+      .then(res => {
+        console.log(res.data)
+        setData(res.data.filter((c: CustomerType) => c._id === id)[0])
+      })
+      .catch(err => console.log(err))
   }
 
   useEffect(() => {
