64b45295bb94edac161e8802b239a03b46006795
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-08-26 23:53:08 +0100

    addScanEmail

---
+++ app/addScanEmail/[_id]/page.tsx
@@ -0,0 +1,55 @@
+'use client'
+
+import { CustomerType, UseBack, TextInput, UseForm } from '@components'
+
+const AddContact = ({ params }: { params: CustomerType }) => {
+  const { formData, handleInputChange, handleSubmit } = UseForm({
+    hostname: '',
+    username: '',
+    password: '',
+    port: '',
+    api: 'insertScanEmail',
+    cid: params._id || '',
+  })
+
+  return (
+    <>
+      <h5>Add Device</h5>
+      <form onSubmit={handleSubmit}>
+        <fieldset>
+          <legend>Device info</legend>
+          <TextInput
+            name="hostname"
+            value={formData.hostname}
+            required
+            onChange={handleInputChange}
+          />
+          <TextInput
+            name="username"
+            value={formData.username}
+            required
+            onChange={handleInputChange}
+          />
+          <TextInput
+            type="password"
+            name="password"
+            value={formData.password}
+            required
+            onChange={handleInputChange}
+          />
+          <TextInput
+            type="text"
+            name="port"
+            value={formData.port}
+            required
+            onChange={handleInputChange}
+          />
+        </fieldset>
+        <button type="submit">Submit</button>
+      </form>
+      <UseBack />
+    </>
+  )
+}
+
+export default AddContact
