1ba79f829b7ac522268cb5eda40e981c13580ee2
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-08-26 12:26:06 +0100

    conditional dns

--- components/network.tsx
+++ components/network.tsx
@@ -1,44 +1,62 @@
 // prettier-ignore
-import { Edit, UserPass, MethodInfoType, useState, ToggleHook, useEffect } from '@components'
+import { Edit, Plus, UserPass, MethodInfoType, useState, ToggleHook, useEffect, NetworkType } from '@components'
 import URL from '@components/Url'
 import { useRouter } from 'next/navigation'
 
-const NetworkConfig = () => {
+const NetworkConfig = ({ network, cid }: { network: NetworkType[]; cid: string }) => {
   const router = useRouter()
-  const [showMethod, setshowMethod] = useState(true)
+  const [showSection, setshowSection] = useState(true)
 
   const handleShowChange = (show: boolean) => {
-    setshowMethod(show)
+    setshowSection(show)
   }
+
+  const DnsSplit = () => {
+    const dns = network[0].dns
+    if (dns) {
+      const dnsArr = dns.split(',')
+      return dnsArr.map(dn => <input type="text" value={dn} />)
+    }
+  }
+
+  const space = <Plus className="text-orange-100 hover:text-orange-100 disabled" />
+
   return (
-    <div className="method bg-yellow-100  border-yellow-400">
+    <div className="method bg-green-100  border-green-400 ">
       <ToggleHook
         onShowChange={handleShowChange}
-        name="Network Config"
-        compo="addMethod"
-        length={1}
+        name="Network"
+        length={network.length}
+        compo="addNetwork"
+        cid={cid}
       />
-      <div className={`transIn ${!showMethod && 'transOut'}`}>
-        <div className="sub">
-          <div className=" bg-yellow-100">
-            <div className="flex justify-between">
-              <Edit onClick={() => router.push(`/editMethod/2/2`)} />
-              <span className="tracking-[4px] bold">{'Network'}</span>
-            </div>
-            <div className="item">
-              <label htmlFor="domain">domain</label>
-              <input type="text" />
-            </div>
-            <div className="item">
-              <label htmlFor="dns">dns</label>
-              <div className="flex flex-col">
-                <input type="text" />
-                <input type="text" />
-                <input type="text" />
+      <div className={`transIn ${!showSection && 'transOut'}`}>
+        {network.map(net => {
+          return (
+            <div key={net._id} className="sub">
+              <div className=" bg-green-100">
+                <div className="flex justify-between">
+                  <Edit onClick={() => router.push(`/editMethod/2/2`)} />
+                  <span className="tracking-[4px] bold">{'Network'}</span>
+                </div>
+                <div className="item">
+                  <label htmlFor="name">name</label>
+                  <input type="text" value={net.name} readOnly />
+                </div>
+                <div className="item">
+                  <label htmlFor="domain">domain</label>
+                  <input type="text" value={net.domain} readOnly />
+                </div>
+                <div className="item">
+                  <label htmlFor="dns">dns</label>
+                  <div className="flex flex-col">
+                    <DnsSplit />
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
-        </div>
+          )
+        })}
       </div>
     </div>
   )
