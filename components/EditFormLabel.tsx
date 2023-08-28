ca5a3c023b9381e37b996cd278c837e17cb1b9a9
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-08-28 22:35:58 +0100

    wrap all sections

--- components/EditFormLabel.tsx
+++ components/EditFormLabel.tsx
@@ -1,5 +1,5 @@
 //prettier-ignore
-import { BackButton, useAppSelector } from '@components'
+import { BackButton, useAppSelector, useRouter } from '@components'
 
 const EditFormLabel = ({
   label,
@@ -10,41 +10,66 @@ const EditFormLabel = ({
   cid: string
   children: React.ReactNode
 }) => {
+  const router = useRouter()
   const customer = useAppSelector(state => state.customer.customers).filter(
     c => c._id === cid
   )[0]
 
+  if (!customer) {
+    return (
+      <div className="error">
+        <h5>
+          ID <i>{cid}</i> not found.
+        </h5>
+        <button onClick={() => router.push('/')}>home</button>
+      </div>
+    )
+  }
+
   return (
-    <div className="mt-6 place-self-center w-full sm:w-3/5 md:w-2/5 flex flex-col ">
+    <div className="mt-6 place-self-center w-full sm:w-[450px]  flex flex-col ">
       <div className="flex justify-center w-full">
         <span
           className={`rounded-l-md border p-2 border-r-0
-        ${label === 'Network' && 'bg-green-200 border-green-300'}
-        ${label === 'Scan-to-folder' && 'bg-indigo-200 border-indigo-300'}
-        ${label === 'Scan-to-email' && 'bg-red-200 border-red-300'}
-        ${label === 'Device' && 'bg-orange-200 border-orange-300'}
-        ${label === 'Server' && 'bg-lime-200 border-lime-300'}
-        ${label === 'Contact' && 'bg-blue-200 border-blue-300'}
-        ${label === 'Remote Method' && 'bg-yellow-200 border-yellow-300'}
+        ${label.includes('Network') && 'networkDark'}
+        ${label.includes('Folder') && 'folderDark'}
+        ${label.includes('Email') && 'emailDark'}
+        ${label.includes('Device') && 'deviceDark'}
+        ${label.includes('Server') && 'serverDark'}
+        ${label.includes('Contact') && 'contactDark'}
+        ${label.includes('Method') && 'methodDark'}
         `}
         >
           {customer.name}
         </span>
         <span
-          className={`rounded-r-md border p-2
-        ${label === 'Network' && 'bg-green-100 border-green-300'}
-        ${label === 'Scan-to-folder' && 'bg-indigo-100 border-indigo-300'}
-        ${label === 'Scan-to-email' && 'bg-red-100 border-red-300'}
-        ${label === 'Device' && 'bg-orange-100 border-orange-300'}
-        ${label === 'Server' && 'bg-lime-100 border-lime-300'}
-        ${label === 'Contact' && 'bg-blue-100 border-blue-300'}
-        ${label === 'Remote Method' && 'bg-yellow-100 border-yellow-300'}
+          className={`rounded-r-md border p-2 
+          ${label.includes('Method') && 'methodLight'}
+          ${label.includes('Server') && 'serverLight'}
+          ${label.includes('Network') && 'networkLight'}
+          ${label.includes('Folder') && 'folderLight'}
+          ${label.includes('Email') && 'emailLight'}
+          ${label.includes('Device') && 'deviceLight'}
+          ${label.includes('Contact') && 'contactLight'}
         `}
         >
           {label}
         </span>
       </div>
-      {children}
+      <div
+        className={`editForm border
+        ${label.includes('Server') && 'serverLight'}
+        ${label.includes('Method') && 'methodLight'}
+        ${label.includes('Network') && 'networkLight'}
+        ${label.includes('Folder') && 'folderLight'}
+        ${label.includes('Email') && 'emailLight'}
+        ${label.includes('Device') && 'deviceLight'}
+        ${label.includes('Contact') && 'contactLight'}
+      `}
+      >
+        {children}
+      </div>
+      <BackButton cid={cid} />
     </div>
   )
 }
