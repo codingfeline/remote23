24f392b6cc6c4bc98ed64cd7618276e07d92439c
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-08-28 22:26:50 +0100

    addCustomer

--- app/addCustomer/page.tsx
+++ app/addCustomer/page.tsx
@@ -4,7 +4,7 @@ import { useState, useEffect } from 'react'
 import Input from '@components/Input'
 // import axios from 'axios'
 import axios from '@components/axios'
-import { TextInput } from '@components'
+import { SubmitButton, TextInput } from '@components'
 import { useRouter } from 'next/navigation'
 import { useAppDispatch, fetchCustomers } from '@components'
 
@@ -75,18 +75,21 @@ const AddCustomer = () => {
   const addSolution = (e: React.FormEvent) => {
     e.preventDefault()
     const lower = solutions.map(s => s.toLocaleLowerCase())
-    if (lower.includes(newSolution.toLowerCase())) {
-      console.log('name exists')
-    } else {
-      axios.post('solutions', { name: newSolution }).then(res => {
-        console.log(res)
-        if (res.statusText === 'Created') {
-          console.log('ok')
-          fetchSolutions()
-          setSelected(newSolution)
-          setShow(false)
-        }
-      })
+    if (newSolution !== '') {
+      if (lower.includes(newSolution.toLowerCase())) {
+        console.log('name exists')
+      } else {
+        axios.post('solutions', { name: newSolution }).then(res => {
+          console.log(res)
+          if (res.statusText === 'Created') {
+            console.log('ok')
+            fetchSolutions()
+            setSelected(newSolution)
+            setShow(false)
+            setNewSolution('')
+          }
+        })
+      }
     }
   }
 
@@ -109,54 +112,51 @@ const AddCustomer = () => {
   }
 
   return (
-    <>
-      <h3>AddCustomer</h3>
-      <form onSubmit={handleSubmit}>
-        <fieldset>
-          <legend>New Customer</legend>
-
-          <TextInput
-            name="name"
-            value={name}
+    <div className="editForm mt-6 place-self-center w-full sm:w-[550px]  flex flex-col ">
+      <span className="place-self-center bg-blue-200 border border-blue-300 rounded-md p-3 ">
+        Add New Customer
+      </span>
+      <form onSubmit={handleSubmit} className="bg-blue-100 border border-blue-200">
+        <TextInput
+          name="name"
+          value={name}
+          required
+          onChange={e => setName(e.target.value)}
+        />
+        <div className="item">
+          <label htmlFor="solution">solution</label>
+          <select
+            name="solution"
+            id="solution"
+            onChange={handleSelect}
+            value={selected}
             required
-            onChange={e => setName(e.target.value)}
-          />
-          <div className="item">
-            <label htmlFor="solution">solution</label>
-            <select
-              name="solution"
-              id="solution"
-              onChange={handleSelect}
-              value={selected}
-              required
-            >
-              <option value="">Choose one</option>
-              {solutions.map(sol => (
-                <option key={sol} value={sol}>
-                  {sol}
-                </option>
-              ))}
-              <option value="new">add new</option>
-            </select>
-          </div>
-
-          {show && (
-            <div className="w-full flex justify-center items-center">
-              <form action="" className="bg-lime-100 w-1/2 p-6 rounded-md">
-                <Input
-                  name="name"
-                  value={newSolution}
-                  handleChange={e => setNewSolution(e.target.value)}
-                />
-                <button onClick={addSolution}>Add</button>
-                <button onClick={() => cancel()}>Cancel</button>
-              </form>
-            </div>
-          )}
-        </fieldset>
-        <div className="flex justify-around">
-          <button>Submit</button>
+          >
+            <option value="">Choose one</option>
+            {solutions.map(sol => (
+              <option key={sol} value={sol}>
+                {sol}
+              </option>
+            ))}
+            <option value="new">add new</option>
+          </select>
         </div>
+
+        {show && (
+          <div className="w-full flex justify-end items-center">
+            <form action="" className="bg-gray-100 w-4/5 p-6 rounded-md">
+              <Input
+                name="name"
+                value={newSolution}
+                handleChange={e => setNewSolution(e.target.value)}
+                required
+              />
+              <button onClick={addSolution}>Add</button>
+              <button onClick={() => cancel()}>Cancel</button>
+            </form>
+          </div>
+        )}
+        <SubmitButton />
       </form>
       {added && (
         <div className="flex flex-col justify-center items-center w-1/3 m-auto mt-8 bg-yellow-100 p-3 rounded-md">
@@ -166,7 +166,7 @@ const AddCustomer = () => {
           </button>
         </div>
       )}
-    </>
+    </div>
   )
 }
 
