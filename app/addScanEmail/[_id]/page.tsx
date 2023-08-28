4c8e18dacbad35b9786f9238ffe05a144d85cdfb
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-08-28 22:33:36 +0100

    wrapped with EditFormLabel

--- app/addScanEmail/[_id]/page.tsx
+++ app/addScanEmail/[_id]/page.tsx
@@ -1,54 +1,50 @@
 'use client'
+//prettier-ignore
+import { TextInput, UseForm, EditFormLabel, SubmitButton } from '@components'
 
-import { CustomerType, UseBack, TextInput, UseForm } from '@components'
-
-const AddContact = ({ params }: { params: CustomerType }) => {
+const AddContact = ({ params }: { params: { _id: string } }) => {
+  const cid = params._id
   const { formData, handleInputChange, handleSubmit } = UseForm({
     hostname: '',
     username: '',
     password: '',
     port: '',
     api: 'insertScanEmail',
-    cid: params._id || '',
+    cid,
   })
 
   return (
-    <>
-      <h5>Add Device</h5>
+    <EditFormLabel cid={cid} label="Add Scan-to-Email">
       <form onSubmit={handleSubmit}>
-        <fieldset>
-          <legend>Device info</legend>
-          <TextInput
-            name="hostname"
-            value={formData.hostname}
-            required
-            onChange={handleInputChange}
-          />
-          <TextInput
-            name="username"
-            value={formData.username}
-            required
-            onChange={handleInputChange}
-          />
-          <TextInput
-            type="password"
-            name="password"
-            value={formData.password}
-            required
-            onChange={handleInputChange}
-          />
-          <TextInput
-            type="text"
-            name="port"
-            value={formData.port}
-            required
-            onChange={handleInputChange}
-          />
-        </fieldset>
-        <button type="submit">Submit</button>
+        <TextInput
+          name="hostname"
+          value={formData.hostname}
+          required
+          onChange={handleInputChange}
+        />
+        <TextInput
+          name="username"
+          value={formData.username}
+          required
+          onChange={handleInputChange}
+        />
+        <TextInput
+          type="password"
+          name="password"
+          value={formData.password}
+          required
+          onChange={handleInputChange}
+        />
+        <TextInput
+          type="text"
+          name="port"
+          value={formData.port}
+          required
+          onChange={handleInputChange}
+        />
+        <SubmitButton />
       </form>
-      <UseBack />
-    </>
+    </EditFormLabel>
   )
 }
 
