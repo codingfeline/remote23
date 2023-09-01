35b1446d72f2f35f8527e3620e9153a335a929d0
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-09-01 22:09:04 +0100

    rename proptype

--- components/addCustomer/AddSolution.tsx
+++ components/addCustomer/AddSolution.tsx
@@ -1,9 +1,8 @@
-import { CheckCircle, Cross, ChangeEvent, FormEvent } from '@components'
+import { CheckCircle, Cross, ChangeEvent, FormEvent, useState, axios } from '@components'
 
-type PropsType = {
+type AddSolutionPropsType = {
   newSolution: string
   handleSolChange: (e: ChangeEvent<HTMLInputElement>) => void
-  placeholder: string
   newSolBlank: boolean
   solNameExists: boolean
   addSolution: (e: FormEvent) => void
@@ -12,40 +11,41 @@ type PropsType = {
 }
 
 const AddSolution = ({
-  newSolution,
-  handleSolChange,
-  placeholder,
-  newSolBlank,
-  solNameExists,
   newSolErr,
   cancel,
+  newSolBlank,
+  solNameExists,
+  newSolution,
+  handleSolChange,
   addSolution,
-}: PropsType) => {
+}: AddSolutionPropsType) => {
   return (
-    <form className={`bg-indigo-200 border border-indigo-300 w-3/5 py-2 rounded-md`}>
-      <div className="flex flex-col">
-        <input
-          type="text"
-          value={newSolution}
-          onChange={handleSolChange}
-          required
-          placeholder={placeholder}
-          className={` ${
-            (newSolBlank || solNameExists) && 'bg-red-200 border border-red-600'
-          }`}
-        />
-      </div>
-      <div className="flex justify-center items-center p-2">
-        <CheckCircle
-          onClick={addSolution}
-          title="Add New Solution"
-          className={`hover:text-green-600 text-[40px] ${
-            newSolErr && 'text-green-600 hover:animate-none motion-safe:animate-bounce'
-          }`}
-        />
-        <Cross onClick={cancel} title="Cancel" className="text-[40px]" />
-      </div>
-    </form>
+    <div className="w-full flex justify-end items-center">
+      <form className={`bg-indigo-200 border border-indigo-300 w-3/5 py-2 rounded-md`}>
+        <div className="flex flex-col">
+          <input
+            type="text"
+            value={newSolution}
+            onChange={handleSolChange}
+            required
+            placeholder={newSolBlank ? 'enter new solution' : ''}
+            className={` ${
+              (newSolBlank || solNameExists) && 'bg-red-200 border border-red-600'
+            }`}
+          />
+        </div>
+        <div className="flex justify-center items-center p-2">
+          <CheckCircle
+            onClick={addSolution}
+            title="Add New Solution"
+            className={`hover:text-green-600 text-[40px] ${
+              newSolErr && 'text-green-600 hover:animate-none motion-safe:animate-bounce'
+            }`}
+          />
+          <Cross onClick={cancel} title="Cancel" className="text-[40px]" />
+        </div>
+      </form>
+    </div>
   )
 }
 
