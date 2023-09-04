717a263dbbeffe7b7229108cbe63f06373da9d66
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-09-04 05:50:50 +0100

    addSolution using redux

--- components/axios.ts
+++ components/axios.ts
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: 'https://remoteapi.in-kent.uk/api'
+  baseURL: 'http://localhost:3121/api'
 })
 
 export default instance
+// baseURL: 'https://remoteapi.in-kent.uk/api'
