c05237cff0fdf70148888643a2cb531127e96677
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-09-10 22:07:08 +0100

    test with dup name and dup solution

--- components/addCustomer/Error.tsx
+++ components/addCustomer/Error.tsx
@@ -1,11 +1,22 @@
 import React from 'react'
 
-const ItemExists = ({ item }: { item: string }) => {
+type ErrorProp = {
+  name?: string
+  both?: string
+  item?: string
+}
+
+const ItemExists = ({ name, both, item }: ErrorProp) => {
   return (
     <div className="flex justify-center ">
       {/* <label htmlFor=""></label> */}
-      <div className="flex  justify-center mt-2 text-red-400 bg-red-100 rounded-md p-2 border border-red-200">
-        <i>{item}</i>
+      <div className="flex w-2/3 justify-center text-center mt-2 text-red-400 bg-red-100 rounded-md p-2 border border-red-200">
+        {item && (
+          <>
+            <i>{item}</i>&nbsp;already exists
+          </>
+        )}
+        {both && 'please fill in both and name must be more than 3 characters'}
       </div>
     </div>
   )
