de1807e7a676f5f30cc57380bf975cc9d9ea9d63
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-09-12 06:44:14 +0100

    reusable submitReset

---
+++ app/addCustomer/SubmitReset.tsx
@@ -0,0 +1,23 @@
+import { Bin, FormEvent, Send } from '@components'
+
+type SubmitProps = {
+  cancel: () => void
+  handleSubmit: (e: FormEvent) => void
+}
+
+const SubmitReset = ({ cancel, handleSubmit }: SubmitProps) => {
+  return (
+    <div className="flex justify-around pt-2">
+      <div className="group submitGroup" onClick={cancel}>
+        <Bin onClick={cancel} title="Reset" className="submitSub" />
+        <span className="submitSub">Reset</span>
+      </div>
+      <div className="group submitGroup" onClick={handleSubmit} title="Add Customer">
+        <Send onClick={handleSubmit} className="submitSub" />
+        <span className="submitSub">Add</span>
+      </div>
+    </div>
+  )
+}
+
+export default SubmitReset
