c7ab9ae75d6f7bb3d9ec02b5c0280779ed6c3738
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-09-24 13:01:59 +0100

    remove logs

--- components/CustomerName.tsx
+++ components/CustomerName.tsx
@@ -1,18 +1,26 @@
 import React, { useEffect, useState } from 'react'
 import { CustomerType } from '@utils/types/customer'
 import axios from '@components/axios'
+import { useRouter } from 'next/navigation'
 
 const CustomerName = ({ id }: { id: string }) => {
+  const router = useRouter()
   const [data, setData] = useState<CustomerType>({} as CustomerType)
 
   const fetchData = async () => {
     await axios
       .get('customers/nameId')
       .then(res => {
-        console.log(res.data)
+        if (!res.data) alert('error')
         setData(res.data.filter((c: CustomerType) => c._id === id)[0])
       })
-      .catch(err => console.log(err))
+      .catch(err => {
+        router.push('/')
+        console.log(err)
+      })
+  }
+  if (!data) {
+    return 'error'
   }
 
   useEffect(() => {
