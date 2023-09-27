3863903f9e8c861ac5e14bfdfb4dbb037bfb19fa
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-09-27 21:12:12 +0100

    project name

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,13 +1,13 @@
 const Footer = () => {
   return (
-    <footer className="p-2 mt-5 bg-slate-200">
+    <footer className="p-2  bg-blue-100 border border-t border-blue-200">
       <ul
         className=" 
         items-center 
         tracking-widest md:tracking-normal 
       "
       >
-        <li>&copy; {new Date().getFullYear()}</li>
+        <li>&copy; {new Date().getFullYear()} Technical Helpdesk CMS</li>
       </ul>
     </footer>
   )
