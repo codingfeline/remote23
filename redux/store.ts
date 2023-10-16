893cc575f8683db788928d3483ec567231ca807c
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-10-16 21:26:51 +0100

    more slices

--- redux/store.ts
+++ redux/store.ts
@@ -4,14 +4,24 @@ import { configureStore } from '@reduxjs/toolkit'
 // import cakeReducer from '@redux/features/cake/cakeSlice'
 // import icecreamReducer from '../features/icecream/icecreamSlice'
 // import userReducer from '../features/user/userSlice'
-import counterReducer from '@redux/features/counter/counterSlice'
-import customerSlice from './features/customers/customerSlice'
+// import copySlice from './features/copy/copySlice'
+import userSlice from './features/user/userSlice'
+import counterSlice from '@redux/features/counter/counterSlice'
+import customerSlice from '@redux/features/customer/customerSlice'
+import solutionSlice from '@redux/features/solution/solutionSlice'
+// import { solutionsApi } from '@services/solutionsApi'
+import { extendedSolutionSlice } from '@query/features/solution/solutionSlice'
+import { apiSlice } from '@query/features/api/apiSlice'
 
 const store = configureStore({
   reducer: {
-    counter: counterReducer,
-    customer: customerSlice
-  }
+    customer: customerSlice,
+    solution: solutionSlice,
+    counter: counterSlice,
+    user: userSlice,
+    [apiSlice.reducerPath]: apiSlice.reducer
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware)
 })
 
 export default store
